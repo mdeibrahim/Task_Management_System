0e160378bd1b472599331535ce477bc0e5e315dd
mdeibrahim <mmeibrahim505@gmail.com>
2025-06-09 14:41:36 +0600

    update settings

--- task_management/settings.py
+++ task_management/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='mmeibrahim505@gmail.com'
-EMAIL_HOST_PASSWORD = 'nexz jcme zecd nnxr'
+EMAIL_HOST_USER =config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
 
 
 FRONTEND_URL = 'http://127.0.0.1:8000'
